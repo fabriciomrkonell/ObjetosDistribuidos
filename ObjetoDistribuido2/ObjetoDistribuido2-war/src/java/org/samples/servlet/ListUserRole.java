9bad35497de45b5647ccb052bab666317e036c45
Fabrício Ronchi <fabricio_konell@hotmail.com>
2014-05-07 19:04:03 -0300

    Up

--- ObjetoDistribuido2/ObjetoDistribuido2-war/src/java/org/samples/servlet/ListUserRole.java
+++ ObjetoDistribuido2/ObjetoDistribuido2-war/src/java/org/samples/servlet/ListUserRole.java
@@ -25,27 +25,39 @@ public class ListUserRole extends HttpServlet {
              List<UserRole> users = facadeLocal.findUserRoleEntities();                   
             response.setContentType("text/html;charset=UTF-8");
             PrintWriter out = response.getWriter();
-            out.println("<!DOCTYPE html>");
+           out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ListRole</title>");            
+            out.println("<link rel='stylesheet' href='http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'>");
+            out.println("<title>List User Role</title>");            
             out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Regras:</h1>");
-            out.println("<table>");
+            out.println("<body>");            
+            out.println("<div class='container' style='margin-top: 20px'>");
+            out.println("<div class='row'>");            
+            out.println("<div class='col-sm-12'>");
+            out.println("<div class='text-center'>");
+            out.println("<marquee><h1>Users - Roles</h1>");
+            out.println("</marquee>");
+            out.println("</div>");
+            out.println("<table class='table table-condensed table-bordered'>");
             out.println("<thead>");
             out.println("<tr>");
-            out.println("<th>Regra</th>");
-            out.println("<th>Usuário</th>");
+            out.println("<th style='text-align: center'>ID</th>");
+            out.println("<th style='text-align: center'>User</th>");
+            out.println("<th style='text-align: center'>Role</th>");
             out.println("</tr>");
             out.println("</thead>");
+            out.println("</div>");
+            out.println("</div>");
+            out.println("</div>");
             out.println("<tbody>");            
             UserRole o;
             for (int i = 0; i < users.size(); i++) {
                 o = (UserRole) users.get(i);
                 out.println("<tr style='text-align: center'>");
-                out.println("<td>" + o.getRoleId() +"</td>");
+                out.println("<td>" + o.getId()+"</td>");
                 out.println("<td>" + o.getUserId()+ "</td>");
+                out.println("<td>" + o.getRoleId()+ "</td>");
                 out.println("</tr>");
                 
             }
