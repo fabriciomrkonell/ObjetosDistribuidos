6ba616a6abfd3524b2fb35ef9b453a3dddc6b1b9
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-05-22 18:55:37 -0300

    FInal

--- ObjetoDistribuido3/SorteadorTESTAR-RMI/src/org/ftd/samples/ejb/testes/TestarRMI.java
+++ ObjetoDistribuido3/SorteadorTESTAR-RMI/src/org/ftd/samples/ejb/testes/TestarRMI.java
@@ -23,40 +23,32 @@ public class TestarRMI {
         try {
             props.setProperty("java.naming.factory.initial",
                     "com.sun.enterprise.naming.SerialInitContextFactory");
-
             props.setProperty("java.naming.factory.url.pkgs",
                     "com.sun.enterprise.naming");
-
             props.setProperty("java.naming.factory.state",
                     "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
-
-            // optional.  Defaults to localhost.  Only needed if web server is running 
-            // on a different host than the appserver    
-            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
-
-            // optional.  Defaults to 3700.  Only needed if target orb port is not 3700.
+            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");           
             props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
 
-            ctx = new InitialContext(props);            
+            ctx = new InitialContext(props);                        
+            UserFacadeRemote user = (UserFacadeRemote) ctx.lookup(JNDI_NAME + "UserFacade");                
+            RoleFacadeRemote role = (RoleFacadeRemote) ctx.lookup(JNDI_NAME + "RoleFacade");    
+                      
             
-            UserFacadeRemote user = (UserFacadeRemote) ctx.lookup(JNDI_NAME + "UserFacade");    
-            List<User> listuser = user.findAll();
-            
+            List<User> listuser = user.findAll();            
             User u;
             System.out.println("---Imprimindo Usuários---");
             for (int i = 0; i < listuser.size(); i++) {
                 u = (User) listuser.get(i);
                 System.out.println("ID: " + u.getId() + " - Nome: " + u.getName());                
             }  
-            System.out.println("Total de Usuários: " + user.count());
+            System.out.println("Total de Usuários: " + user.count());                                    
             
             System.out.println("");
             System.out.println("------");
             System.out.println("");
-            
-            RoleFacadeRemote role = (RoleFacadeRemote) ctx.lookup(JNDI_NAME + "RoleFacade");    
-            List<Role> listrole = role.findAll();
-            
+                        
+            List<Role> listrole = role.findAll();            
             Role r;
             System.out.println("---Imprimindo Regras---");
             for (int i = 0; i < listrole.size(); i++) {
@@ -68,7 +60,6 @@ public class TestarRMI {
         } catch (NamingException ex) {
             Logger.getLogger(TestarRMI.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-    }
-
+    }        
+    
 }
